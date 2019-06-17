83cd979f9f654f2504cc84ff1cb175a574e838a7
Anthony Mackay <amk@gegroup.com.au>
2019-06-17 11:56:03 +1000

    Debug print response headers

--- pyhaystack/client/http/sync.py
+++ pyhaystack/client/http/sync.py
@@ -51,6 +51,7 @@ class SyncHttpClient(HTTPClient):
                             auth=auth, timeout=timeout,
                             proxies=proxies, verify=tls_verify,
                             cert=tls_cert)
+                    print(response.headers)
                     if (accept_status is None) or \
                             (response.status_code not in accept_status):
                         response.raise_for_status()
